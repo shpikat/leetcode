c7026f1883883c32ae3746d50c7652586b20da36
Alexei Shpikat <a.shpikat@gmail.com>
2022-09-05 20:51:57 +0300

    Update solution to be O(n)
    Trades complexity for memory, relying on hashed map implementation to be
     O(1).

--- leetcode/0001-two-sum.go
+++ leetcode/0001-two-sum.go
@@ -1,33 +1,14 @@
 package leetcode
 
-import (
-	"sort"
-)
-
 func twoSum(nums []int, target int) []int {
-	sorted := make([]int, len(nums))
-	copy(sorted, nums)
-	sort.Ints(sorted)
-	left := 0
-	right := len(sorted) - 1
-	for {
-		leftValue := sorted[left]
-		rightValue := sorted[right]
-		sum := leftValue + rightValue
-		if sum < target {
-			left++
-		} else if sum > target {
-			right--
+	known := make(map[int]int, len(nums))
+	for i, n := range nums {
+		j, exists := known[target-n]
+		if exists {
+			return []int{i, j}
 		} else {
-			answer := make([]int, 0, 2)
-			for i, value := range nums {
-				if value == leftValue || value == rightValue {
-					answer = append(answer, i)
-					if len(answer) == 2 {
-						return answer
-					}
-				}
-			}
+			known[n] = i
 		}
 	}
+	return []int{}
 }
