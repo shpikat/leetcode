01144259ef5e93fa1a3aa849ccd602f476a06f5f
Alexei Shpikat <a.shpikat@gmail.com>
2022-11-16 11:58:57 +0300

    Solve Maximum Depth of Binary Tree
    https://leetcode.com/problems/maximum-depth-of-binary-tree/
    
    Given the root of a binary tree, return its maximum depth.
    
    A binary tree's maximum depth is the number of nodes along the longest
    path from the root node down to the farthest leaf node.

---
+++ leetcode/0104-maximum-depth-of-binary-tree.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
